3c3c92c48062ea2679dba4cb20c23b5e2b6647a6
yeohhq <yeohhuiqing00@gmail.com>
2022-10-26 21:11:19 +0800

    Remove up-down button for number inputs

--- src/problem2/fancy-form/src/components/OTP/OTPModal.js
+++ src/problem2/fancy-form/src/components/OTP/OTPModal.js
@@ -14,7 +14,7 @@ import _ from 'lodash'
 export default function OTPModal({ disabled, setHasRequestedOTP }) {
 	const [open, setOpen] = useState(false)
 	const [mobileNo, setMobileNo] = useState('')
-	// const [isValidMobileNo, setIsValidMobileNo] = useState(true)
+	const [isValidMobileNo, setIsValidMobileNo] = useState(true)
 	const handleOpen = () => setOpen(true)
 	const handleClose = () => setOpen(false)
 
@@ -27,7 +27,7 @@ export default function OTPModal({ disabled, setHasRequestedOTP }) {
 		e.preventDefault()
 
 		if (!_.isNumber(mobileNo) && mobileNo.length !== 8) {
-			// setIsValidMobileNo(false)
+			setIsValidMobileNo(false)
 		} else {
 			// Valid mobile no
 			setOpen(false)
@@ -44,7 +44,7 @@ export default function OTPModal({ disabled, setHasRequestedOTP }) {
 				<DialogTitle>Request for OTP</DialogTitle>
 				<DialogContent>
 					<DialogContentText>
-						Please enter your mobile number
+						Please enter your 8-digit mobile number
 					</DialogContentText>
 					<TextField
 						autoFocus
@@ -53,7 +53,7 @@ export default function OTPModal({ disabled, setHasRequestedOTP }) {
 						label="Mobile Number"
 						type="number"
 						fullWidth
-						vali
+						error={!isValidMobileNo}
 						onChange={handleOnMobileChange}
 					/>
 				</DialogContent>
