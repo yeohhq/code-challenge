b2442aee9e81b1148f9966d95f33b4c561b8c9e9
yeohhq <yeohhuiqing00@gmail.com>
2022-10-26 20:20:03 +0800

    Fix imports

--- fancy-form/src/components/OTP/OTPInput.js
+++ fancy-form/src/components/OTP/OTPInput.js
@@ -22,13 +22,6 @@ export const OTPInput = (props) => {
 	const [activeInput, setActiveInput] = useState(0)
 	const [otpValues, setOTPValues] = useState(emptyOtp)
 
-	// Reset form values
-	useEffect(() => {
-		console.log('resetting')
-		handleOtpChange(emptyOtp)
-		setOTPValues(emptyOtp)
-	}, [reset, emptyOtp, handleOtpChange])
-
 	// Helper to return OTP from inputs
 	const handleOtpChange = useCallback(
 		(otp) => {
@@ -37,6 +30,12 @@ export const OTPInput = (props) => {
 		},
 		[onChangeOTP]
 	)
+
+	// Reset form values
+	useEffect(() => {
+		handleOtpChange(emptyOtp)
+		setOTPValues(emptyOtp)
+	}, [reset, emptyOtp, handleOtpChange])
 
 	// Focus `inputIndex` input
 	const focusInput = useCallback(
